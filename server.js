e8dd3220238608ea40ff9e02dab00053b6ec6a45
Crystal Odi <crystalodi@gmail.com>
2021-08-17 15:59:31 -0500

    prepare for heroku

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const {mongoUri, PORT} = require("./config");
 const cors = require("cors");
 const morgan = require("morgan");
 const rewardsRoutes = require("./routes/api/rewards");
+const path = require("path");
 
 app.use(cors());
 app.use(morgan("tiny"));
@@ -22,6 +23,11 @@ mongoose.connect(mongoUri, {
 .then(() => console.log("MongoDB database connected")).catch((err) => console.log(err));
 
 app.use('/api/rewards', rewardsRoutes)
-app.get("/", (req, res) => res.send("Hello World"));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/dist"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+    })
+}
 app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
 
